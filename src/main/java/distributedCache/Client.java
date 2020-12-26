4fcde71bb47121d45e6fe0fe8ba9bee71dfdd144
ilya karkin <karkinia@yandex.ru>
2020-12-26 19:58:50 +0300

    Scripted auto-commit on change (2020-12-26 19:58:50) by gitwatch.sh

--- src/main/java/distributedCache/Client.java
+++ src/main/java/distributedCache/Client.java
@@ -1,4 +1,15 @@
 package distributedCache;
 
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 public class Client {
+
+    public static final int THREADS_COUNT = 1;
+    public static final int TIMEOUT = 3000;
+
+    public static void main(String[] argv) {
+        ZContext context = new ZContext(THREADS_COUNT);
+        ZMQ.Socket client = createAndConnect(context, "tcp://localhost:8086", TIMEOUT);
+    }
 }
