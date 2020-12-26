66a6d586892f8fca04505549705ce7e4fa94ac39
ilya karkin <karkinia@yandex.ru>
2020-12-26 19:59:58 +0300

    Scripted auto-commit on change (2020-12-26 19:59:58) by gitwatch.sh

--- src/main/java/distributedCache/Client.java
+++ src/main/java/distributedCache/Client.java
@@ -7,9 +7,12 @@ public class Client {
 
     public static final int THREADS_COUNT = 1;
     public static final int TIMEOUT = 3000;
+    public static final String SERVER_ADDRESS = "tcp://localhost:8086";
 
     public static void main(String[] argv) {
         ZContext context = new ZContext(THREADS_COUNT);
-        ZMQ.Socket client = createAndConnect(context, "tcp://localhost:8086", TIMEOUT);
+        ZMQ.Socket client = connect(context, SERVER_ADDRESS, TIMEOUT);
     }
+
+    public static  ZMQ.Socket connect(ZContext context, String addres)
 }
