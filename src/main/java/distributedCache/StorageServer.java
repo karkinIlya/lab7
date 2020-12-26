ecfe4873448e9a64979d7a53d2fbac4098ae9fce
ilya karkin <karkinia@yandex.ru>
2020-12-26 21:27:43 +0300

    Scripted auto-commit on change (2020-12-26 21:27:43) by gitwatch.sh

--- src/main/java/distributedCache/StorageServer.java
+++ src/main/java/distributedCache/StorageServer.java
@@ -10,8 +10,9 @@ import java.util.Arrays;
 public class StorageServer {
 
     public static final String SERVER_ADDRESS = "tcp://localhost:8088";
-    public static final int TIMEOUT = 5000;
+    public static final int TIMEOUT = 1000;
     private static final ArrayList<Cache> caches = new ArrayList();
+    public static final int GLOBAL_TIMEOUT = 3000;
 
     public static void main(String[] argv) {
         int start = Integer.parseInt(argv[0]);
@@ -24,8 +25,9 @@ public class StorageServer {
         ZMQ.Poller poller = context.createPoller(1);
         poller.register(dealerSocket, ZMQ.Poller.POLLIN);
         long time = System.currentTimeMillis();
-        while(poller.poll(3000) != -1) {
+        while(poller.poll(GLOBAL_TIMEOUT) != -1) {
             if (System.currentTimeMillis() - time >= TIMEOUT) {
+                dealerSocket.send("notify " + id + " " + start + " " + end);
             }
         }
         context.destroySocket(dealerSocket);
