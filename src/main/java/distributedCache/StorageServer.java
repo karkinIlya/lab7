92625ed26413bee3e45b18cbb94cedefc4cdfbfe
ilya karkin <karkinia@yandex.ru>
2020-12-26 21:31:20 +0300

    Scripted auto-commit on change (2020-12-26 21:31:20) by gitwatch.sh

--- src/main/java/distributedCache/StorageServer.java
+++ src/main/java/distributedCache/StorageServer.java
@@ -3,9 +3,12 @@ package distributedCache;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.UUID;
 
 public class StorageServer {
 
@@ -13,6 +16,9 @@ public class StorageServer {
     public static final int TIMEOUT = 1000;
     private static final ArrayList<Cache> caches = new ArrayList();
     public static final int GLOBAL_TIMEOUT = 3000;
+    private static final String id = UUID.randomUUID().toString();
+    public static final int CLIENT_SOCKET = 0;
+    public static final String GET_REQUEST = "get";
 
     public static void main(String[] argv) {
         int start = Integer.parseInt(argv[0]);
@@ -28,6 +34,15 @@ public class StorageServer {
         while(poller.poll(GLOBAL_TIMEOUT) != -1) {
             if (System.currentTimeMillis() - time >= TIMEOUT) {
                 dealerSocket.send("notify " + id + " " + start + " " + end);
+                time = System.currentTimeMillis();
+            }
+            if (poller.pollin(CLIENT_SOCKET)) {
+                ZMsg msg = ZMsg.recvMsg(dealerSocket);
+                String stringMessage = msg.getLast().toString();
+                if(stringMessage.contains(GET_REQUEST)) {
+                    int index = Integer.parseInt(stringMessage.split(" ")[1]);
+                    msg.getLast().reset()
+                }
             }
         }
         context.destroySocket(dealerSocket);
