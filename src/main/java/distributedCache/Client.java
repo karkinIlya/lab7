a719931a64a5047c6a803f9db9ae85dd9d70ce26
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:08:52 +0300

    Scripted auto-commit on change (2020-12-26 20:08:52) by gitwatch.sh

--- src/main/java/distributedCache/Client.java
+++ src/main/java/distributedCache/Client.java
@@ -12,21 +12,32 @@ public class Client {
     public static final int TIMEOUT = 3000;
     public static final String SERVER_ADDRESS = "tcp://localhost:8086";
 
-    public static void main(String[] argv) {
-        ZContext context = new ZContext(THREADS_COUNT);
-        ZMQ.Socket client = connectToServer(context);
-        Scanner in = new Scanner(System.in);
-        while (true) {
-
-        }
-        context.destroySocket(client);
-        context.destroy();
-    }
-
     public static  ZMQ.Socket connectToServer(ZContext context) {
         ZMQ.Socket socket = context.createSocket(SocketType.REQ);
         socket.setReceiveTimeOut(TIMEOUT);
         socket.connect(SERVER_ADDRESS);
         return socket;
+    }
+
+    public static void main(String[] argv) {
+        ZContext context = new ZContext(THREADS_COUNT);
+        ZMQ.Socket client = connectToServer(context);
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String cmd = in.nextLine();
+            if (cmd == "quit") {
+                break;
+            }
+            client.send(cmd);
+            String tmp = client.recvStr();
+            if (tmp != null) {
+                System.out.println(tmp);
+            } else {
+                context.destroySocket(client);
+                client = connectToServer(context);
+            }
+        }
+        context.destroySocket(client);
+        context.destroy();
     }
 }
