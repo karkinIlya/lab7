58e5e06e609a05839e0ea1863bda12525664cb35
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:44:42 +0300

    Scripted auto-commit on change (2020-12-26 20:44:42) by gitwatch.sh

--- src/main/java/distributedCache/StorageServer.java
+++ src/main/java/distributedCache/StorageServer.java
@@ -1,5 +1,6 @@
 package distributedCache;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
@@ -8,13 +9,16 @@ import java.util.Arrays;
 
 public class StorageServer {
 
+    public static final String SERVER_ADDRESS = "tcp://localhost:8088";
+
     public static void main(String[] argv) {
         int start = Integer.parseInt(argv[0]);
         ArrayList<String> cache = new ArrayList<>(Arrays.asList(argv).subList(1, argv.length));
         int end = start + cache.size();
 
         ZContext context = new ZContext(1);
-        
+        ZMQ.Socket dealerSocket = context.createSocket(SocketType.DEALER);
+        dealerSocket.connect(SERVER_ADDRESS);
         ZMQ.Poller poller = context.createPoller(1);
         while(poller.poll(3000) != -1) {
 
