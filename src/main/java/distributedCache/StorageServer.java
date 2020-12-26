b6dd094984bff7c8a7d07e480dfb4ec164ca2ba2
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:40:22 +0300

    Scripted auto-commit on change (2020-12-26 20:40:22) by gitwatch.sh

--- src/main/java/distributedCache/StorageServer.java
+++ src/main/java/distributedCache/StorageServer.java
@@ -1,9 +1,18 @@
 package distributedCache;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StorageServer {
 
     public static void main(String[] argv) {
         int start = Integer.parseInt(argv[0]);
-        int end = start
+        ArrayList<String> cache = new ArrayList<>(Arrays.asList(argv).subList(1, argv.length));
+        int end = start + cache.size();
+
+        
+        while(poller.poll(3000) != -1) {
+
+        }
     }
 }
