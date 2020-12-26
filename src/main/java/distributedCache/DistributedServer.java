290b2f6a5ab1f6ae898170a96b947fe5c6842ce4
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:29:09 +0300

    Scripted auto-commit on change (2020-12-26 20:29:09) by gitwatch.sh

--- src/main/java/distributedCache/DistributedServer.java
+++ src/main/java/distributedCache/DistributedServer.java
@@ -1,7 +1,16 @@
 package distributedCache;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 public class DistributedServer {
+
+    public static final int THREADS_COUNT = 1;
+
     public static void main(String[] argv) {
-        
+        ZContext context = new ZContext(THREADS_COUNT);
+        ZMQ.Socket clientSocket = context.createSocket(SocketType.ROUTER);
+        clientSocket.bind()
     }
 }
