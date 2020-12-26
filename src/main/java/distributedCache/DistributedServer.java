c6e4ce180195cb60e21f84dfa2b582c34011f598
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:32:42 +0300

    Scripted auto-commit on change (2020-12-26 20:32:42) by gitwatch.sh

--- src/main/java/distributedCache/DistributedServer.java
+++ src/main/java/distributedCache/DistributedServer.java
@@ -7,10 +7,14 @@ import org.zeromq.ZMQ;
 public class DistributedServer {
 
     public static final int THREADS_COUNT = 1;
+    public static final String CLIENT_SERVER = "tcp://localhost:8086";
+    public static final String STORAGE_SERVER = "tcp://localhost:8086";
 
     public static void main(String[] argv) {
         ZContext context = new ZContext(THREADS_COUNT);
         ZMQ.Socket clientSocket = context.createSocket(SocketType.ROUTER);
-        clientSocket.bind("tcp://localhost:8086");
+        clientSocket.bind(CLIENT_SERVER);
+        ZMQ.Socket storageSocket = context.createSocket(SocketType.ROUTER);
+        storageSocket.bind(STORAGE_SERVER);
     }
 }
