7f402e56333be1b0e052996e944161d7b9748d4b
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:55:22 +0300

    Scripted auto-commit on change (2020-12-26 20:55:22) by gitwatch.sh

--- src/main/java/distributedCache/Cache.java
+++ src/main/java/distributedCache/Cache.java
@@ -1,4 +1,21 @@
 package distributedCache;
 
+import org.zeromq.ZFrame;
+
 public class Cache {
+    private long time;
+    private int start;
+    private int end;
+    private ZFrame frame;
+    private String id;
+
+    public Cache(int start, int end, String id, ZFrame frame) {
+        this.time = System.currentTimeMillis();
+        this. start = start;
+        this.end = end;
+        this.frame = frame;
+        this.id = id;
+    }
+
+    boolean 
 }
