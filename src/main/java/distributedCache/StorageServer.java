d5bbe8f77541758de321bb754965e3b47c832da4
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:38:24 +0300

    Scripted auto-commit on change (2020-12-26 20:38:24) by gitwatch.sh

--- src/main/java/distributedCache/StorageServer.java
+++ src/main/java/distributedCache/StorageServer.java
@@ -1,4 +1,9 @@
 package distributedCache;
 
 public class StorageServer {
+
+    public static void main(String[] argv) {
+        int start = Integer.parseInt(argv[0]);
+        int end = start
+    }
 }
