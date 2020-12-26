a22da50ceb3a17ee3c664c5314e19cdf0c23696c
ilya karkin <karkinia@yandex.ru>
2020-12-26 20:35:26 +0300

    Scripted auto-commit on change (2020-12-26 20:35:26) by gitwatch.sh

--- src/main/java/distributedCache/DistributedServer.java
+++ src/main/java/distributedCache/DistributedServer.java
@@ -9,6 +9,8 @@ public class DistributedServer {
     public static final int THREADS_COUNT = 1;
     public static final String CLIENT_SERVER = "tcp://localhost:8086";
     public static final String STORAGE_SERVER = "tcp://localhost:8086";
+    public static final int POLLER_SIZE = 2;
+    public static final int TIMEOUT = 5000;
 
     public static void main(String[] argv) {
         ZContext context = new ZContext(THREADS_COUNT);
@@ -16,5 +18,11 @@ public class DistributedServer {
         clientSocket.bind(CLIENT_SERVER);
         ZMQ.Socket storageSocket = context.createSocket(SocketType.ROUTER);
         storageSocket.bind(STORAGE_SERVER);
+        ZMQ.Poller poller = context.createPoller(POLLER_SIZE);
+        poller.register(clientSocket, ZMQ.Poller.POLLIN);
+        poller.register(storageSocket, ZMQ.Poller.POLLIN);
+        long time = System.currentTimeMillis();
+        while (poller.poll(TIMEOUT))
+
     }
 }
